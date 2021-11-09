f78fbdeafc15ba1ff7631563bdd9c98db548d2d8
MD: Suvo <santo35702@gmail.com>
2021-11-09 17:25:19 +0600

    Admin: Brand & Category Delete

--- app/Http/Livewire/Admin/BrandPage.php
+++ app/Http/Livewire/Admin/BrandPage.php
@@ -4,12 +4,20 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use App\Models\Brand;
+use Illuminate\Http\Request;
 
 class BrandPage extends Component
 {
+    public function deleteItem($id, Request $request)
+    {
+        $brand = Brand::find($id);
+        $brand->delete();
+        $request->session()->flash('status', 'Brand has been deleted successfully!');
+    }
+    
     public function render()
     {
-        $brands = Brand::paginate();
+        $brands = Brand::get();
         return view('livewire.admin.brand-page', ['brands' => $brands])->layout('layouts.admin');
     }
 }
--- app/Http/Livewire/Admin/CategoryPage.php
+++ app/Http/Livewire/Admin/CategoryPage.php
@@ -4,9 +4,17 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoryPage extends Component
 {
+    public function deleteItem($id, Request $request)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        $request->session()->flash('status', 'Category has been deleted successfully!');
+    }
+
     public function render()
     {
         $categories = Category::get();
