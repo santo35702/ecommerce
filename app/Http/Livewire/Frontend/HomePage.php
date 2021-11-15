ccebed1246915584a8d70338246f5c012681e764
MD: Suvo <santo35702@gmail.com>
2021-11-15 11:07:36 +0600

    Dynamic Carousel show in Home Page

--- app/Http/Livewire/Frontend/HomePage.php
+++ app/Http/Livewire/Frontend/HomePage.php
@@ -4,12 +4,14 @@ namespace App\Http\Livewire\Frontend;
 
 use Livewire\Component;
 use App\Models\Brand;
+use App\Models\HomeSlider;
 
 class HomePage extends Component
 {
     public function render()
     {
+        $carousel = HomeSlider::where('status', 1)->inRandomOrder()->get();
         $brands = Brand::inRandomOrder()->limit(10)->get();
-        return view('livewire.frontend.home-page', ['brands' => $brands])->layout('layouts.base');
+        return view('livewire.frontend.home-page', ['brands' => $brands, 'carousel' => $carousel])->layout('layouts.base');
     }
 }
