2c5175d603ca0e02d96cce45989c764316520679
MD: Suvo <santo35702@gmail.com>
2021-11-15 12:37:27 +0600

    New arrival show in home page

--- app/Http/Livewire/Frontend/HomePage.php
+++ app/Http/Livewire/Frontend/HomePage.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Frontend;
 
 use Livewire\Component;
 use App\Models\Brand;
+use App\Models\Product;
 use App\Models\HomeSlider;
 
 class HomePage extends Component
@@ -11,7 +12,8 @@ class HomePage extends Component
     public function render()
     {
         $carousel = HomeSlider::where('status', 1)->inRandomOrder()->get();
+        $new_arrival_all = Product::orderBy('created_at', 'Desc')->get()->take(10);
         $brands = Brand::inRandomOrder()->limit(10)->get();
-        return view('livewire.frontend.home-page', ['brands' => $brands, 'carousel' => $carousel])->layout('layouts.base');
+        return view('livewire.frontend.home-page', ['carousel' => $carousel, 'new_arrival_all' => $new_arrival_all, 'brands' => $brands])->layout('layouts.base');
     }
 }
--- resources/views/livewire/frontend/home-page.blade.php
+++ resources/views/livewire/frontend/home-page.blade.php
@@ -32,12 +32,98 @@
                     </div>
                     <div class="tabs-listing">
                         <ul class="tabs clearfix">
-                            <li class="active" rel="tab1">Women</li>
-                            <li rel="tab2">Men</li>
-                            <li rel="tab3">Sale</li>
+                            <li class="active" rel="tab1">All</li>
+                            <li rel="tab2">Women</li>
+                            <li rel="tab3">Men</li>
+                            <li rel="tab4">Sale</li>
                         </ul>
                         <div class="tab_container">
                             <div id="tab1" class="tab_content grid-products">
+                                <div class="productSlider">
+                                    @foreach ($new_arrival_all as $key)
+                                        <div class="col-12 item grid-view-item--sold-out">
+                                            <!-- start product image -->
+                                            <div class="product-image">
+                                                <!-- start product image -->
+                                                <a href="{{ route('products.details', $key->slug) }}">
+                                                    <!-- image -->
+                                                    <img class="primary blur-up lazyload" data-src="{{ asset('assets/images/product-images/' . $key->image ) }}" src="{{ asset('assets/images/product-images/' . $key->image ) }}" alt="{{ $key->title }}" title="{{ $key->title }}">
+                                                    <!-- End image -->
+                                                    <!-- Hover image -->
+                                                    <img class="hover blur-up lazyload" data-src="{{ asset('assets/images/product-images/product-image1-1.jpg') }}" src="{{ asset('assets/images/product-images/product-image1-1.jpg') }}" alt="{{ $key->title }}" title="{{ $key->title }}">
+                                                    <!-- End hover image -->
+                                                    <!-- product label -->
+                                                    <div class="product-labels rectangular"><span class="lbl on-sale">-16%</span> <span class="lbl pr-label1">new</span> <span class="lbl pr-label2">Hot</span> <span class="lbl on-sale">Sale</span> <span class="lbl pr-label3">Popular</span></div>
+                                                    <span class="sold-out"><span>Sold out</span></span>
+                                                    <!-- End product label -->
+                                                </a>
+                                                <!-- end product image -->
+
+                                                <!-- countdown start -->
+                                                <div class="saleTime desktop" data-countdown="2022/03/01"></div>
+                                                <!-- countdown end -->
+
+                                                <!-- Start product button -->
+                                                <a href="#" class="variants add btn btn-addto-cart" wire:click.prevent="store({{ $key->id }}, '{{ $key->title }}', {{ $key->regular_price }})">Add To Cart</a>
+                                                <div class="button-set">
+                                                    <a href="javascript:void(0)" title="Quick View" class="quick-view-popup quick-view" data-toggle="modal" data-target="#content_quickview">
+                                                        <i class="icon anm anm-search-plus-r"></i>
+                                                    </a>
+                                                    <div class="wishlist-btn">
+                                                        <a class="wishlist add-to-wishlist" href="#" title="Add to Wishlist">
+                                                            <i class="icon anm anm-heart-l"></i>
+                                                        </a>
+                                                    </div>
+                                                    <div class="compare-btn">
+                                                        <a class="compare add-to-compare" href="compare.html" title="Add to Compare">
+                                                            <i class="icon anm anm-random-r"></i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                                <!-- end product button -->
+                                            </div>
+                                            <!-- end product image -->
+
+                                            <!--start product details -->
+                                            <div class="product-details text-center">
+                                                <!-- product name -->
+                                                <div class="product-name text-capitalize">
+                                                    <a href="{{ route('products.details', $key->slug) }}">{{ $key->title }}</a>
+                                                </div>
+                                                <!-- End product name -->
+                                                <!-- product price -->
+                                                <div class="product-price">
+                                                    <span class="old-price">${{ $key->regular_price }}</span>
+                                                    <span class="price">${{ $key->regular_price }}</span>
+                                                </div>
+                                                <!-- End product price -->
+
+                                                <div class="product-review">
+                                                    <i class="font-13 fa fa-star"></i>
+                                                    <i class="font-13 fa fa-star"></i>
+                                                    <i class="font-13 fa fa-star"></i>
+                                                    <i class="font-13 fa fa-star-o"></i>
+                                                    <i class="font-13 fa fa-star-o"></i>
+                                                </div>
+                                                <!-- Variant -->
+                                                <ul class="swatches">
+                                                    <li class="swatch medium rounded"><img src="{{ asset('assets/images/product-images/variant1.jpg') }}" alt="image" /></li>
+                                                    <li class="swatch medium rounded"><img src="{{ asset('assets/images/product-images/variant2.jpg') }}" alt="image" /></li>
+                                                    <li class="swatch medium rounded"><img src="{{ asset('assets/images/product-images/variant3.jpg') }}" alt="image" /></li>
+                                                    <li class="swatch medium rounded"><img src="{{ asset('assets/images/product-images/variant4.jpg') }}" alt="image" /></li>
+                                                    <li class="swatch medium rounded"><img src="{{ asset('assets/images/product-images/variant5.jpg') }}" alt="image" /></li>
+                                                </ul>
+                                                <!-- End Variant -->
+                                            </div>
+                                            <!-- End product details -->
+                                            <!-- countdown start -->
+                                            <div class="timermobile"><div class="saleTime desktop" data-countdown="2022/03/01"></div></div>
+                                            <!-- countdown end -->
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                            <div id="tab2" class="tab_content grid-products">
                                 <div class="productSlider">
                                     <div class="col-12 item">
                                         <!-- start product image -->
@@ -399,7 +485,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div id="tab2" class="tab_content grid-products">
+                            <div id="tab3" class="tab_content grid-products">
                                 <div class="productSlider">
                                     <div class="col-12 item">
                                         <!-- start product image -->
@@ -706,7 +792,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div id="tab3" class="tab_content grid-products">
+                            <div id="tab4" class="tab_content grid-products">
                                 <div class="productSlider">
                                     <div class="col-12 item">
                                         <!-- start product image -->
